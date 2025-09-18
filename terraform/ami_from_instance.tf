ad370904bda89fe4dc07b12476273af1dbc864bf
patanfouzi <patanfouzi26@gmail.com>
2025-09-18 13:24:18 +0530

    Update ami_from_instance.tf

--- terraform/ami_from_instance.tf
+++ terraform/ami_from_instance.tf
@@ -1,7 +1,9 @@
 resource "aws_ami_from_instance" "from_instance" {
-  name               = "${var.project}-ami-${timestamp()}"
-  source_instance_id = var.source_instance_id
-  description        = "AMI created from ${var.source_instance_id}"
-  snapshot_without_reboot = true   # non-disruptive snapshot; set false if you want clean state with restart
-  tags = { Name = "${var.project}-ami" }
+  name               = "myapp-prod-ami-${formatdate("YYYY-MM-DD-HH-MM-SS", timestamp())}"
+  source_instance_id = "i-08e862327bb75aa71"
+  snapshot_without_reboot = true
+
+  tags = {
+    Name = "myapp-prod-ami"
+  }
 }
