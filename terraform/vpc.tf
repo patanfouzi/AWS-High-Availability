7d575430c455256458d7e253ebc237ef53fa47c0
patanfouzi <patanfouzi26@gmail.com>
2025-09-18 13:17:56 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -89,12 +89,12 @@ resource "aws_route_table" "public" {
   }
 }
 
+# Public route table associations
 resource "aws_route_table_association" "public_assoc" {
-  for_each      = aws_subnet.public
-  subnet_id     = each.value.id
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
-
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
@@ -108,8 +108,9 @@ resource "aws_route_table" "private" {
   }
 }
 
+# Private route table associations
 resource "aws_route_table_association" "private_assoc" {
-  for_each      = aws_subnet.private
-  subnet_id     = each.value.id
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
