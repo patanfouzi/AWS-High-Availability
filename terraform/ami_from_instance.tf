3e1f0ea4f3eea438d42ae901a5b761c3658bd682
patanfouzi <patanfouzi26@gmail.com>
2025-09-18 13:27:39 +0530

    Update ami_from_instance.tf

--- terraform/ami_from_instance.tf
+++ terraform/ami_from_instance.tf
@@ -1,5 +1,5 @@
 resource "aws_ami_from_instance" "from_instance" {
-  name               = "myapp-prod-ami-${formatdate("YYYY-MM-DD-HH-MM-SS", timestamp())}"
+  name               = "$(project_id)-ami-${formatdate("YYYY-MM-DD-HH-MM-SS", timestamp())}"
   source_instance_id = var.source_instance_id
   snapshot_without_reboot = true
 
