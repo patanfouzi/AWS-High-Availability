6ca9d813a3b965aa2510171ce30c671563dc5fb1
patanfouzi <patanfouzi26@gmail.com>
2025-09-18 12:35:13 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -42,7 +42,10 @@ resource "aws_nat_gateway" "nat" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
-  route { cidr_block = "0.0.0.0/0"; gateway_id = aws_internet_gateway.igw.id }
+  route { 
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = { Name = "${var.project}-public-rt" }
 }
 
@@ -54,7 +57,9 @@ resource "aws_route_table_association" "public_assoc" {
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
-  route { cidr_block = "0.0.0.0/0"; nat_gateway_id = aws_nat_gateway.nat.id }
+  route { 
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id }
   tags = { Name = "${var.project}-private-rt" }
 }
 
