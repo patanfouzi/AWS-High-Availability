6ff1322950001d4df5cd258ced6ec63261b0627c
patanfouzi <patanfouzi26@gmail.com>
2025-09-18 18:09:41 +0530

    Update launch_template.tf

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -4,7 +4,15 @@
   instance_type = "t2.micro"
   key_name      = var.key_name  
   vpc_security_group_ids = [aws_security_group.app_sg.id]
-  user_data = filebase64("${path.module}/userdata.sh")
+
+  user_data = base64encode(<<EOF
+#!/bin/bash
+aws s3 cp s3://cloudwatch-script-bkt/userdata.sh /tmp/userdata.sh
+chmod +x /tmp/userdata.sh
+/tmp/userdata.sh
+EOF
+  )
+
   iam_instance_profile {
     arn = aws_iam_instance_profile.ec2_profile.arn
   }
