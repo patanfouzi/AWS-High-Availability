31ab5945d97b933496381dd0cae7c1a7537dd375
patanfouzi <patanfouzi26@gmail.com>
2025-09-18 17:36:17 +0530

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -4,7 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
 apt-get upgrade -y
-
 apt-get install -y nginx
 systemctl enable nginx
 systemctl start nginx
@@ -13,39 +12,16 @@ wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-
 dpkg -i /tmp/amazon-cloudwatch-agent.deb || true
 apt-get -f install -y || true
 
-cat <<'JSON' > /opt/aws/amazon-cloudwatch-agent/bin/config.json
-{
-  "agent": {
-    "metrics_collection_interval": 60,
-    "logfile": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"
-  },
-  "metrics": {
-    "metrics_collected": {
-      "cpu": {
-        "measurement": [
-          {"name": "cpu_usage_idle", "rename": "CPU_IDLE", "unit": "Percent"},
-          {"name": "cpu_usage_system", "rename": "CPU_SYS", "unit": "Percent"},
-          {"name": "cpu_usage_user", "rename": "CPU_USER", "unit": "Percent"}
-        ],
-        "metrics_collection_interval": 60,
-        "totalcpu": true
-      },
-      "mem": {
-        "measurement": [
-          {"name": "mem_used_percent", "unit": "Percent"}
-        ],
-        "metrics_collection_interval": 60
-      },
-      "disk": {
-        "measurement": [
-          {"name": "used_percent", "unit": "Percent"}
-        ],
-        "metrics_collection_interval": 60,
-        "resources": ["*"]
-      }
-    }
-  }
-}
+cat <<'JSON' >/opt/aws/amazon-cloudwatch-agent/bin/config.json
+{"agent":{"metrics_collection_interval":60,"logfile":"/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"},
+"metrics":{"metrics_collected":{
+"cpu":{"measurement":[{"name":"cpu_usage_idle","rename":"CPU_IDLE","unit":"Percent"},
+{"name":"cpu_usage_system","rename":"CPU_SYS","unit":"Percent"},
+{"name":"cpu_usage_user","rename":"CPU_USER","unit":"Percent"}],
+"metrics_collection_interval":60,"totalcpu":true},
+"mem":{"measurement":[{"name":"mem_used_percent","unit":"Percent"}],"metrics_collection_interval":60},
+"disk":{"measurement":[{"name":"used_percent","unit":"Percent"}],"metrics_collection_interval":60,"resources":["*"]}
+}}}
 JSON
 
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s || true
