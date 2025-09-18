dbf45b6155e22b1b3b8a9ddf308516f15bf689a0
patanfouzi <patanfouzi26@gmail.com>
2025-09-18 12:39:41 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -9,15 +9,19 @@ data "aws_iam_policy" "ssm_policy" {
 data "aws_iam_policy_document" "ec2_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals { type = "Service"
-    identifiers = ["ec2.amazonaws.com"] }
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"] 
+    }
   }
 }
 
 resource "aws_iam_role" "ec2_role" {
   name = "${var.project}-ec2-role"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-  tags = { Name = "${var.project}-ec2-role" }
+  tags = { 
+    Name = "${var.project}-ec2-role" 
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "attach_cw" {
