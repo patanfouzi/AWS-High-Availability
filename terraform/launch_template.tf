186615657cea5cab63f742af3a6b2b724b7983e2
patanfouzi <patanfouzi26@gmail.com>
2025-09-18 16:03:08 +0530

    Update launch_template.tf

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -4,7 +4,64 @@
   instance_type = "t2.micro"
   key_name      = var.key_name  
   vpc_security_group_ids = [aws_security_group.app_sg.id]
-  user_data     = file("${path.module}/userdata.sh")
+  # user_data     = file("${path.module}/userdata.sh")
+ 
+  # Terraform automatically encodes this, so DO NOT wrap in base64encode()
+  user_data = <<-EOT
+    #!/bin/bash
+    set -e
+    export DEBIAN_FRONTEND=noninteractive
+
+    apt-get update -y
+    apt-get upgrade -y
+
+    apt-get install -y nginx
+    systemctl enable nginx
+    systemctl start nginx
+
+    wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb -O /tmp/amazon-cloudwatch-agent.deb
+    dpkg -i /tmp/amazon-cloudwatch-agent.deb || true
+    apt-get -f install -y || true
+
+    cat <<'JSON' > /opt/aws/amazon-cloudwatch-agent/bin/config.json
+    {
+      "agent": {
+        "metrics_collection_interval": 60,
+        "logfile": "/opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log"
+      },
+      "metrics": {
+        "metrics_collected": {
+          "cpu": {
+            "measurement": [
+              {"name": "cpu_usage_idle", "rename": "CPU_IDLE", "unit": "Percent"},
+              {"name": "cpu_usage_system", "rename": "CPU_SYS", "unit": "Percent"},
+              {"name": "cpu_usage_user", "rename": "CPU_USER", "unit": "Percent"}
+            ],
+            "metrics_collection_interval": 60,
+            "totalcpu": true
+          },
+          "mem": {
+            "measurement": [
+              {"name": "mem_used_percent", "unit": "Percent"}
+            ],
+            "metrics_collection_interval": 60
+          },
+          "disk": {
+            "measurement": [
+              {"name": "used_percent", "unit": "Percent"}
+            ],
+            "metrics_collection_interval": 60,
+            "resources": ["*"]
+          }
+        }
+      }
+    }
+    JSON
+
+    /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s || true
+  EOT
+
+
   iam_instance_profile {
     name = aws_iam_instance_profile.ec2_profile.name
   }
