c6cbd3cb323d4bb96df02b9b01de2b8ccc3e8cbd
patanfouzi <patanfouzi26@gmail.com>
2025-10-11 21:09:24 +0530

    Update launch_template.tf

--- terraform/launch_template.tf
+++ terraform/launch_template.tf
@@ -1,24 +1,12 @@
- resource "aws_launch_template" "app_lt" {
-  name   = "${var.project}-lt"
+resource "aws_launch_template" "app_lt" {                                                                    
+  update_default_version = true
   image_id      = aws_ami_from_instance.from_instance.id
-  instance_type = "t2.micro" 
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
-
-  user_data = base64encode(<<EOF
-#!/bin/bash
-# Log output
-exec > /var/log/user-data.log 2>&1
-set -x
-# Download and run the script from S3
-aws s3 cp s3://cloudwatch-script-bkt/userdata.sh /tmp/userdata.sh
-chmod +x /tmp/userdata.sh
-bash /tmp/userdata.sh
-EOF
-)
-
-
+  key_name= var.key_name
+  user_data = base64encode(file("${path.module}/scripts/user_data.sh"))
   iam_instance_profile {
-    arn = aws_iam_instance_profile.ec2_profile.arn
+    name = aws_iam_instance_profile.ec2_profile.name
   }
 
   tag_specifications {
