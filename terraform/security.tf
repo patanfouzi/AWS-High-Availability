931c19f20bb953f73eb1e8d20ad85a3db1a800cb
patanfouzi <patanfouzi26@gmail.com>
2025-10-11 21:10:48 +0530

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,29 +1,30 @@
 # ALB SG
 resource "aws_security_group" "alb_sg" {
-  name   = "${var.project}-alb-sg"
-  vpc_id = aws_vpc.this.id
+  name        = "${var.project}-alb-sg"
+  vpc_id      = aws_vpc.this.id
   description = "Allow 80/443 inbound from internet"
 
   ingress {
     description = "HTTP"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     description = "HTTPS"
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     # if cert not provided, you can remove 443
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -34,36 +35,25 @@ resource "aws_security_group" "alb_sg" {
 
 # App / Instance SG (only allow from ALB)
 resource "aws_security_group" "app_sg" {
-  name   = "${var.project}-app-sg"
-  vpc_id = aws_vpc.this.id
+  name        = "${var.project}-app-sg"
+  vpc_id      = aws_vpc.this.id
   description = "Allow traffic from ALB"
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb_sg.id]
   }
 
-  # Optional SSH if you provided a key; restrict CIDR ideally
-  dynamic "ingress" {
-    for_each = var.key_name != "" ? [1] : []
-    content {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = { 
-    Name = "${var.project}-app-sg" 
+  tags = {
+    Name = "${var.project}-app-sg"
   }
 }
