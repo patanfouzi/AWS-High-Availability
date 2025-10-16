24f3417fe71006ce27655464bb3a6420ce9b4d56
patanfouzi <patanfouzi26@gmail.com>
2025-10-16 19:13:06 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-bkt-prod"    # replace
+    bucket         = "my-tf-state-file-backend"    # replace
     key            = "autoscaling/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
